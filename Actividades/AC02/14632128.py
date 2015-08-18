9fa23d6cc26f3b910f20ef5832f7aa88d687bf5f
javierlopeza <jilopez8@uc.cl>
2015-08-18 11:08:39 -0300

    arreglo kwargs

--- Actividades/AC02/14632128.py
+++ Actividades/AC02/14632128.py
@@ -1,5 +1,5 @@
 class Audifono():
-	def __init__(self, min_f, max_f, impedencia, int_max):
+	def __init__(self, min_f, max_f, impedencia, int_max, **kwargs):
 		self.min_f = min_f
 		self.max_f = max_f
 		self.impedencia = impedencia
@@ -9,34 +9,33 @@ class Audifono():
 		print(frase)
 
 class OverEar(Audifono):
-	def __init__(self, min_f, max_f, impedencia, int_max, p_aislacion):
-		super().__init__(min_f, max_f, impedencia, int_max)
+	def __init__(self, p_aislacion, **kwargs):
+		super().__init__(**kwargs)
 		self.p_aislacion = p_aislacion
 
 class Intraaurales(Audifono):
-	def __init__(self, min_f, max_f, impedencia, int_max, p_incomodidad):
-		super().__init__(min_f, max_f, impedencia, int_max)
+	def __init__(self, p_incomodidad, **kwargs):
+		super().__init__(**kwargs)
 		self.p_incomodidad = p_incomodidad
 
 class Inalambrico(Audifono):
-	def __init__(self, min_f, max_f, impedencia, int_max, rango_max):
-		super().__init__(min_f, max_f, impedencia, int_max)
+	def __init__(self, rango_max, **kwargs):
+		super().__init__(**kwargs)
 		self.rango_max = rango_max
 	def escuchar(self, cancion):
 		frase = "La cancion "+cancion+" esta siendo reproducida desde un audifono Inalambrico."
 		print(frase)
 
 class Bluetooth(Inalambrico):
-	def __init__(self, min_f, max_f, impedencia, int_max, rango_max, identificador):
-		super().__init__(min_f, max_f, impedencia, int_max, rango_max)
+	def __init__(self, identificador, **kwargs):
+		super().__init__(**kwargs)
 		self.identificador = identificador
 	def escuchar(self, cancion):
 		frase = "La cancion "+cancion+" esta siendo reproducida desde un audifono con Bluetooth."
 		print(frase)
 
 class Reproductor():
-	def __init__(self):
-		pass
+
 	def conectar_audifono(self, audifono, distancia): 
 	#1. El argumento distancia corresponde a la distancia a la 
 	#	que se encuentra el audifono del reproductor.
@@ -47,9 +46,11 @@ class Reproductor():
 		else:
 			print("Error al conectar el audifono.")
 
-audifono = Audifono(0,10,100,120) #Instancio un Audifono.
-aud_inalambrico = Inalambrico(0,10,100,120,50) #Instancio un Audifono Inalambrico.
-aud_bt = Bluetooth(0,10,100,120,50,123) #Instancio un Audifono Inalambrico Bluetooth.
+audifono = Audifono(min_f=0, max_f=100, impedencia=150, int_max=200) #Instancio un Audifono.
+aud_inalambrico = Inalambrico(min_f=0, max_f=100, impedencia=150, int_max=200, rango_max=50) #Instancio un Audifono Inalambrico.
+aud_bt = Bluetooth(min_f=0, max_f=100, impedencia=150, int_max=200, rango_max=50, identificador=123) #Instancio un Audifono Inalambrico Bluetooth.
+
+over = OverEar(min_f=0, max_f=100, impedencia=150, int_max=200, p_aislacion=85)
 
 audifono.escuchar("Yellow Submarine") #Reproduzco la cancion Yellow Submarine con el Audifono instanciado anteriormente.
 aud_inalambrico.escuchar("We Will Rock You") #Reproduzco la cancion We Will Rock You con el Audifono Inalambrico instanciado anteriormente.
