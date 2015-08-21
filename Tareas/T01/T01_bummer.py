e528f3c7ef4e54ce8e9980c8ebcb413684d95049
javierlopeza <jilopez8@uc.cl>
2015-08-21 17:53:24 -0300

    actualizacion archivos

--- Tareas/T01/T01_bummer.py
+++
@@ -1,6 +0,0 @@
-from clases import Persona, Alumno, Profesor, Horario, Curso, Evaluacion
-
-javier = Alumno(nombre_apellido="Javier Lopez", usuario="jilopez8", clave="jla123", alumno="SI", idolos=["Juan Perez","Jose Gonzalez"], cursos_aprobados=["IIC1103","MAT1620"])
-print(javier.alumno)
-progra = Curso(sigla="IIC1103", profesor="Bellido Jesus", lista_de_alumnos=["a1","a2"], seccion=2, campus="San Joaquin", evaluaciones=[], pre_requisitos=[123], capacidad_maxima=100, hora_cat="L-W-V:3", sala_cat="CS-101", hora_ayud="", sala_ayud="", hora_lab="", sala_lab="") 
-print(progra.profesor.apellido)
