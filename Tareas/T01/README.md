ee03fd7bc3d94cc0c10de2212a9facd8d5f7ae6a
javierlopeza <jilopez8@uc.cl>
2015-08-23 16:51:14 -0300

    update readme, start readfile_cursos.py

--- Tareas/T01/README.md
+++ Tareas/T01/README.md
@@ -11,3 +11,49 @@ Consideraciones al revisar:
     "alumno"
     "usuario"
 ```
+
+* **02.** En archivo *cursos.txt* algunos cursos tienen sus datos en distinto orden a los de otros cursos, por ejemplo,
+```
+#CURSO 1
+  {
+    "disp": 77,
+    "eng": "NO",
+    "apr": "NO",
+    "curso": "Álgebra Lineal",
+    "sala_lab": "(Por Asignar)",
+    "hora_cat": "L-W-V:3",
+    "sala_cat": "CS-101",
+    "hora_ayud": "W:6",
+    "ofr": 77,
+    "ocu": 0,
+    "hora_lab": "W:5",
+    "cred": 10,
+    "sec": 1,
+    "sigla": "MAT1203",
+    "sala_ayud": "M1",
+    "campus": "San Joaquin",
+    "retiro": "SI",
+    "NRC": 14268,
+    "profesor": [
+      "Huerta Ivan",
+      "Becerra Carolina"
+    ]
+  }
+#CURSO 2
+  {
+    "ofr": 79,
+    "ocu": 0,
+    "hora_lab": "L:4,5",
+    "cred": 0,
+    "sec": 1,
+    "disp": 79,
+    "retiro": "SI",
+    "sigla": "FIS0153",
+    "eng": "NO",
+    "apr": "NO",
+    "campus": "San Joaquin",
+    "curso": "Laboratorio de Electricidad y Magnetismo",
+    "sala_lab": "(Por Asignar)",
+    "NRC": 16312,
+    "profesor": "During Gustavo"
+  } 
--- Tareas/T01/readfile_cursos.py
+++ Tareas/T01/readfile_cursos.py
@@ -3,7 +3,54 @@ from clases import Persona, Alumno, Profesor, Horario, Curso, Evaluacion
 
 def cargar_cursos():
     cursos_file = open("cursos.txt", "r", encoding="utf8").readlines()
+    cursos = []
+    for i in range(len(cursos_file)):
+        if cursos_file[i] == '  {\n':
+            for j in range(i, len(cursos_file)):
+            	leido = cursos_file[j].split(":")[0][5:-1]
+            	if leido == "disp":
+            		disponibles = cursos_file[j].split(":")[1][1:-2]
+            	elif leido == "ocu":
+            		ocupados = cursos_file[j].split(":")[1][1:-2]
+            	elif leido == "ofr":
+            		ofrecidos = cursos_file[j].split(":")[1][1:-2]
+            	elif leido == "curso":
+            		nombre = cursos_file[j].split(":")[1][2:-3]
+            	elif leido == "apr":
+            		aprobacion_especial = cursos_file[j].split(":")[1][2:-3]
+            	elif leido == "eng":
+            		eng = cursos_file[j].split(":")[1][2:-3]
+            	elif leido == "cred":
+            		creditos = cursos_file[j].split(":")[1][1:-2]
+            	elif leido == "sec":
+            		seccion = cursos_file[j].split(":")[1][1:-2]
+            	elif leido == "NRC":
+            		nrc = cursos_file[j].split(":")[1][1:-2]
+            	elif leido == "hora_lab":
+            		hora_lab = cursos_file[j].split(":")[1][2:-3]
+            	elif leido == "sala_lab":
+            		sala_lab = cursos_file[j].split(":")[1][2:-3]
+            	elif leido == "hora_cat":
+            		hora_cat = cursos_file[j].split(":")[1][2:-3]
+            	elif leido == "sala_cat":
+            		sala_cat = cursos_file[j].split(":")[1][2:-3]
+            	elif leido == "hora_ayud":
+            		hora_ayud = cursos_file[j].split(":")[1][2:-3]
+            	elif leido == "sala_ayud":
+            		sala_ayud = cursos_file[j].split(":")[1][2:-3]
+            	elif leido == "campus":
+            		campus = cursos_file[j].split(":")[1][2:-3]
+            	elif leido == "sigla":
+            		sigla = cursos_file[j].split(":")[1][2:-3]
+            	elif leido == "retiro":
+            		retiro = cursos_file[j].split(":")[1][2:-3]
 
+            	elif cursos_file[j] == "  },\n":
+            		print("fin curso")
+
+            		break
+
+            print(sigla, retiro)
 
 
 cargar_cursos()
