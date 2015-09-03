c66242398590140764ed5e74e11a5a02d4e30245
javierlopeza <jilopez8@uc.cl>
2015-09-03 11:25:07 -0300

    Ac07

--- Actividades/AC07/main.py
+++ Actividades/AC07/main.py
@@ -5,21 +5,20 @@ from functools import reduce
 class BigAnalizador:
     def __init__(self, logs):
         self.logs = logs
-        print(type(self.logs[0].status))
         self.bytes = 0
         self.errores = 0
         self.exitos = 0
 
     def bytes_transferidos(self):
-        print(reduce(self.bytes.__add__(), map(self.logs, lambda x: x.size)))
+        print(reduce(self.bytes.__add__, map(self.logs, lambda x: x.size)))
 
     def errores_servidor(self):
         errores = (404, 500, 501)
-        print(reduce(self.errores.__add__(), map(self.logs, lambda x: 1 if x.status in errores else 0)))
+        print(reduce(self.errores.__add__, map(self.logs, lambda x: 1 if x.status in errores else 0)))
 
     def solicitudes_exitosas(self):
         exitos = (200, 302, 304)
-        print(reduce(self.exitos.__add__(), map(self.logs, lambda x: 1 if x.status in exitos else 0)))
+        print(reduce(self.exitos.__add__, map(self.logs, lambda x: 1 if x.status in exitos else 0)))
 
     def url_mas_solicitada(self):
         urls = dict()
