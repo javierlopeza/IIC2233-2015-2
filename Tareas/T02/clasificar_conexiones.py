ab7390e7b4b07c2622f6bdb4e940e5bf45a98ce7
javierlopeza <jilopez8@uc.cl>
2015-09-17 21:08:24 -0300

    delete files

--- Tareas/T02/clasificar_conexiones.py
+++
@@ -1,4 +0,0 @@
-def clasificar_conexiones(red_bummer):
-    for p in range(len(red_bummer.puertos)):
-        for c in range(len(red_bummer.puertos[p].conexiones)):
-            red_bummer.puertos[p].conexiones[c].clasificar()
