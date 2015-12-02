3142d2072442692a84492a0127f12fbb6af029cd
javierlopeza <jilopez8@uc.cl>
2015-12-02 13:09:09 -0300

    Auth2 implementado

--- Tareas/T07/LoginWindow.py
+++ Tareas/T07/LoginWindow.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-
 from PyQt4 import QtCore, QtGui, uic
 import dropbox
+from dropbox import DropboxOAuth2FlowNoRedirect
 from MainWindow import MainWindow
 
 ventana = uic.loadUiType("login_window.ui")
@@ -18,24 +18,38 @@ class LoginWindow(ventana[0], ventana[1]):
 
         self.IngresarButton.clicked.connect(self.ingresar_clicked)
 
+        self.auth_flow = DropboxOAuth2FlowNoRedirect("7st46mxndppni46", "iqih79vfzyjdcwq")
+        authorize_url = self.auth_flow.start()
+
+        self.webView.load(QtCore.QUrl(authorize_url))
+
     def ingresar_clicked(self):
-        token_ingresado = self.TokenText.text()
-        if not token_ingresado:
+        auth_code = self.TokenText.text().strip()
+        if not auth_code:
             QtGui.QMessageBox.critical(None,
                                        'ERROR',
                                        'Por favor ingrese su token.',
                                        QtGui.QMessageBox.Ok)
+            return
+        try:
+            access_token, user_id = self.auth_flow.finish(auth_code)
+        except:
+            QtGui.QMessageBox.critical(None,
+                                       'ERROR',
+                                       'Revise el codigo ingresado.',
+                                       QtGui.QMessageBox.Ok)
+            return
+
+        try:
+            dbx = dropbox.Dropbox(access_token)
+
+        except:
+            QtGui.QMessageBox.critical(None,
+                                       'ERROR',
+                                       'No ha sido posible conectarse.',
+                                       QtGui.QMessageBox.Ok)
+
         else:
-            try:
-                dbx = dropbox.Dropbox(token_ingresado)
-
-            except:
-                QtGui.QMessageBox.critical(None,
-                                           'ERROR',
-                                           'No ha sido posible conectarse.',
-                                           QtGui.QMessageBox.Ok)
-
-            else:
-                self.close()
-                self.main_window = MainWindow(dbx)
-                self.main_window.show()
+            self.close()
+            self.main_window = MainWindow(dbx)
+            self.main_window.show()
